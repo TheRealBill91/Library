ab9af2bcdbff2f27ea57f91758b89e8fcd6449e8
Billy <bc35786@gmail.com>
2023-02-21 15:21:50 -0600

    Changes:
    -Add ESLint (standard)
    -Add README.md
    -Add 'eslint-config-prettier' (avoid conflicts)

---
+++ README.md
@@ -0,0 +1,3 @@
+# About
+
+A web application that helps users keep track of the books they're reading. With an intuitive and user-friendly interface built using HTML, CSS, and JavaScript, users can add, edit, and delete books from their reading list. The application stores book information such as title, author, and genre, as well as the user's progress through each book. By using this application, users can keep track of their reading progress and easily pick up where they left off.
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
     this.info = function () {
         if (this.read === true) {
             this.read = 'already read'
@@ -10,8 +10,8 @@ function Book(title, author, pages, read) {
             this.read = 'not read yet'
         };
         return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    };
+    }
 }
 
-const book1 = new Book('Harry Potter', 'J.K. Rowling', 256, true);
-const book2 = new Book('Lightning Thief', 'Some dude', 300, false);
+const book1 = new Book('Harry Potter', 'J.K. Rowling', 256, true)
+const book2 = new Book('Lightning Thief', 'Some dude', 300, false)
